c59e09243ee12ebc2f01c9da5a200e499f1b4420
Justix47 <justix47@gmail.com>
2024-01-28 11:50:11 -0500

    Added Footsteps SFX

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    private FMOD.Studio.EventInstance audioMusic;
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music");
-    }
 
     public void Upgrade()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Upgrade");
+    }
+
+    public void Footstep()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Footstep");
     }
 }
