a8f468bf5d68745f74f295c4502f5085cf80f626
VALSergey <47816038+DragonLife6@users.noreply.github.com>
2024-01-28 03:22:37 +0200

    Conflict fix

--- Assets/Player/Scripts/ProjectileScript.cs
+++ Assets/Player/Scripts/ProjectileScript.cs
@@ -6,14 +6,17 @@ public class ProjectileScript : MonoBehaviour
 {
     float damage = 10f;
     float moveSpeed = 15f;
-  
+
+    int bulletMaxPenetrations = 1;
+    int currentPenetration = 0;
+    float bulletCritChance = 0f;
 
     private void Start()
-    {
+    {   
         //AUDIO
         FMODUnity.RuntimeManager.PlayOneShot("event:/Gunshot");
 
-        Destroy(gameObject, 15f);
+        Destroy(gameObject, 5f);
     }
 
     private void Update()
@@ -23,13 +26,38 @@ public class ProjectileScript : MonoBehaviour
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
     }
 
+    public void SetParameters(float newDamage, float critChance, float penetration)
+    {
+        damage = newDamage;
+        bulletCritChance = critChance;
+        bulletMaxPenetrations = Mathf.FloorToInt(penetration);
+        currentPenetration = 0;
+    }
+
+    private bool CheckCritDamage()
+    {
+        return Random.Range(0f, 1f) <= bulletCritChance;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
         {
-            other.gameObject.SendMessage("ApplyDamage", damage);
-            Destroy(gameObject);
+            
+            currentPenetration++;
+            if (CheckCritDamage())
+            {
+                other.gameObject.SendMessage("ApplyCritDamage", damage * 1.5f);
+            }
+            else
+            {
+                other.gameObject.SendMessage("ApplyDamage", damage);
+            }
+
+            if (currentPenetration >= bulletMaxPenetrations)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
