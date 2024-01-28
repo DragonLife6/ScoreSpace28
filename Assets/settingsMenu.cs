d774408f37d21e2f3b74ccdbed7fdb8266467139
Justix47 <justix47@gmail.com>
2024-01-28 11:26:15 -0500

    Added Menu Music and fixed volume slider

--- Assets/settingsMenu.cs
+++ Assets/settingsMenu.cs
@@ -1,14 +1,36 @@
+using FMOD.Studio;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using FMOD;
+using FMODUnity;
+
 
 public class settingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    // Start is called before the first frame update
-   public void SetVolume( float volume)
+    //public AudioMixer audioMixer;
+    public float mastervolume = 1;
+    private Bus masterBus;
+
+
+    private void Awake()
     {
-        audioMixer.SetFloat("volume",volume);
+        masterBus = RuntimeManager.GetBus("bus:/");
+
+    }
+
+    private void Update()
+    {
+        
+
+    }
+
+    // Start is called before the first frame update
+    public void SetVolume(float mastervolume)
+    {
+        //audioMixer.SetFloat("volume",volume);
+        masterBus.setVolume(mastervolume);
+
     }
 }
